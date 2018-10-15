8aef87403806894e6a05df0504ba2a17fb6e8242
kulkarni-sameer <sameer43445@gmail.com>
2018-10-14 21:07:34 -0700

    input value update

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,14 +2,21 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 
 class SearchBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = { term: ''};
+  }
   render(){
     return (
       <div>
-      <input onChange={(event) => console.log(event.target.value)}/>
+      <input
+       value= {this.state.term}
+      onChange={(event) => this.setState({term: event.target.value})}
+      />
       </div>
     );
   }
-  
+
 }
 
 export default SearchBar;
