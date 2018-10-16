4801a596feb88f45d8b4b0661e1ebc66d9a2992b
kulkarni-sameer <sameer43445@gmail.com>
2018-10-15 23:57:59 -0700

    convert index to class based component

--- src/index.js
+++ src/index.js
@@ -1,28 +1,24 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
-import YTSearch from 'youtube-api-search'
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyB5eCjjGcRTt6tAtnClpEnEipRemHJJS1o';
-
-
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {videos: []};
-    YTSearch({key: API_KEY, term: 'iPhone'}, (videos) =>{
-      this.setState({videos})
-      console.log(videos);
+    this.state = {videos:[]};
+    YTSearch({key: API_KEY, term: 'iPhone'}, (videos) => {
+    this.setState({videos});
     });
   }
-   render(){
-    return (
-      <div>
-      <SearchBar />
-      </div>
-
-    );
-}}
-
-ReactDOM.render(<App/>, document.querySelector(".container"))
+  render(){
+  return(
+    <div>
+    <SearchBar/>
+    </div>
+  );
+}
+}
+ReactDOM.render(<App/>, document.querySelector(".container"));
