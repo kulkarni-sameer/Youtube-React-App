83f7a6ec937d98356fda8c9e633233fc6e5001d6
kulkarni-sameer <sameer43445@gmail.com>
2018-10-21 23:11:27 -0700

    video details add

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,23 +1,16 @@
 import React from 'react';
+import VideoListItem from './video_list_item'
 
-const VideoDetail = ({video}) => {
-  if(!video){
-    return <div>Loading..</div>
-  }
-  const videoId = video.id.videoId;
-  const url = `https://www.youtube.com/embed/${videoId}`;
+const VideoDetail = (props) => {
+  const vid = props.videos.map(function(video){
+    return <VideoListItem video = {video} />
+})
+ return (
+   <div>
+   {vid}
 
-  return (
-    <div className = "video-detail col-md-8">
-    <div className = "embed-responsive embed-responsive-16by9">
-    <iframe className = "embed-responsive-item" src={url}></iframe>
-    </div>
-    <div className = "details">
-    <div>{video.snippet.title}</div>
-    <div>{video.snippet.description}</div>
-    </div>
-    </div>
-  );
-};
+   </div>
+ );
+}
 
 export default VideoDetail;
