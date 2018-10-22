71d11e5da965ffe49f239eb7f2283864162eef65
kulkarni-sameer <sameer43445@gmail.com>
2018-10-21 23:09:13 -0700

    use ES6 syntax

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -9,7 +9,9 @@ class SearchBar extends Component {
   }
   render(){
     return (
-      <input onChange={(event) => this.setState({term: (event.target.value)})}/>
+      <div>
+      <input onChange = {(event) => this.setState({term: event.target.value})}/>
+      </div>
     );
   }
 }
