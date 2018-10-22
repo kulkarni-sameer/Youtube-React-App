b15f6fd9a01cb49cf229307dc45ff337aef582a3
kulkarni-sameer <sameer43445@gmail.com>
2018-10-21 23:14:51 -0700

    Add more details to video item component

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
 
 const VideoListItem = (props) => {
-  const vid = props.video;
-  const imgurl = vid.snippet.thumbnails.default.url;
   return (
-    <li>
-    <img src= {imgurl} />
-    </li>
+    <div>
+    {props.video.snippet.description}
+    </div>
   );
 }
 
