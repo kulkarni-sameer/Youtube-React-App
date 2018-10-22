b80e3a33ddf3044307b4de1594e3b7a0f8bddce6
kulkarni-sameer <sameer43445@gmail.com>
2018-10-21 23:13:25 -0700

    change App to class based component

--- src/index.js
+++ src/index.js
@@ -1,33 +1,26 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar';
+import SearchBar from './components/search_bar'
 import YTSearch from 'youtube-api-search';
-import VideoList from './components/video_list'
 import VideoDetail from './components/video_detail'
 
-const API_KEY = 'AIzaSyB5eCjjGcRTt6tAtnClpEnEipRemHJJS1o';
-
-
-
-class App extends Component{
+class App extends Component {
   constructor(props){
+    const API_KEY = 'AIzaSyB5eCjjGcRTt6tAtnClpEnEipRemHJJS1o';
     super(props);
 
     this.state = {videos: []};
-    YTSearch({key:API_KEY, term:'iPhone'}, (videos) => {
+    YTSearch({key: API_KEY, term: 'iPhone'}, (videos) =>{
       this.setState({videos});
     })
-
   }
   render(){
-    
   return (
     <div>
-    <SearchBar/>
-    <VideoDetail video = {this.state.videos[0]} />
-    <VideoList videos = {this.state.videos} />
+    <SearchBar />
+    <VideoDetail videos = {this.state.videos} />
     </div>
   );
-}}
-
-ReactDOM.render(<App/>, document.querySelector(".container"));
+}
+}
+ReactDOM.render(<App />, document.querySelector(".container"));
