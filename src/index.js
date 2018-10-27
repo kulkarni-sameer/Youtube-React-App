f93d7d3615a114ac28aeb4def409cec188df26e3
kulkarni-sameer <sameer43445@gmail.com>
2018-10-26 23:43:30 -0700

    work towards giving users ability to select videos

--- src/index.js
+++ src/index.js
@@ -10,17 +10,19 @@ class App extends Component {
   constructor(props){
     const API_KEY = 'AIzaSyB5eCjjGcRTt6tAtnClpEnEipRemHJJS1o';
     super(props);
-    this.state = {videos: []};
+    this.state = {videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'iPhone'}, (videos) =>{
-      this.setState({videos});
+      this.setState({videos:videos, selectedVideo: videos[0]});
     })
   }
   render(){
   return (
     <div>
     <SearchBar />
-    <VideoDetail video={this.state.videos[0]} />
+    <VideoDetail video={this.state.selectedVideo} />
     <VideoList videos = {this.state.videos} />
     </div>
   );
